c63ea371f760828cd036e47fef047cdd0c7e6013
Jonas Hegemann <jonas.hegemann@hotmail.de>
2022-01-19 11:24:28 +0100

    update benchmark script

--- script/run-test.sh
+++ script/run-test.sh
@@ -3,8 +3,8 @@
 rm -f data/*
 make clean
 make -j16
-./bin/muonbase-server.app -d -v -c config/muonbase-config.json
-./bin/muonbase-client.app -t -o 1024 -c 1 -n 1
+./bin/muonbase-server.app -d -c config/muonbase-config.json
+./bin/muonbase-client.app -t -o 4096 -c 2 -n 4
 ./script/kill-daemon.sh
 rm -f data/*
 make clean
